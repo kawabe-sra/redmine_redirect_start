1e60865b3577f927ff3fc7793f23d0e87971d2a8
Y. Kawabe <kawabe@sra.co.jp>
2024-06-14 18:03:51 +0900

    Fix for Redmine5.

--- init.rb
+++ init.rb
@@ -1,12 +1,12 @@
 require_dependency 'welcome_controller'
-require_dependency 'redirect_start_patch'
+require File.expand_path('../lib/redmine_redirect_start_patch', __FILE__)
 
 Redmine::Plugin.register :redmine_redirect_start do
   name 'Redmine Redirect Start Page plugin'
   author 'Y. kawabe'
   description 'This redmine plugin redirect redmine start page.'
   author_url 'http://www.projdepot.jp/'
-  version '0.2.0'
+  version '0.3.0'
 
   settings :partial => 'settings/redmine_redirect_start_settings',
     :default => { 'start' => '' }
